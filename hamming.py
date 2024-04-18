92cd278b869fbce558242477581f26422ece2489
Jared Reiss <jared.reiss@comcast.net>
2024-04-17 22:17:26 -0500

    Add function to generate random bit sequence

--- hamming.py
+++ hamming.py
@@ -1,8 +1,31 @@
-from random import random
+import random
+import math
+
+# Generates a random byte length of specified length
+def generateByteSequence(length):
+    # Create empty byte sequence
+    byteSequence = []
+
+    # Generate either a 1 or 0 for specified length
+    for i in range(length):
+        # Generate random number and round to 0 or 1
+        bit = round(random.random())
+
+        # Append bit to byte sequence array
+        byteSequence.append(bit)
+
+    # Return byte sequence
+    return byteSequence
 
 # Main function
 def main():
+    # Print welcome message
     print("Welcome to (15, 11) Extended Hamming Code Implementation!\n")
+
+    # Generate byte sequence
+    data = generateByteSequence(256)
+
+    print(data)
 
 if __name__ == "__main__":
     main()
